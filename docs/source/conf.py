0c4c9dc3285fcbb9b17d36db517a5b7bb918c1ec
Timon Schmelzer <timon.schmelzer@tu-dortmund.de>
2018-11-16 12:18:17 +0100

    Change to googly style syntax

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'  # To use google documentation syntax
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- dopyr/screamer.py
+++ dopyr/screamer.py
@@ -1,11 +1,12 @@
 def maybe_scream(text, do_scream=False):
     """Returns given text input as caps lock text, if do_scream is true.
 
-    :param text: Some input text.
-    :type text: str.
-    :param do_scream: Decide, whether to scream or not.
-    :type do_scream: bool.
-    :returns: str -- May be in caps lock.
+    Args:
+        text (str): Some input text
+        do_scream (bool): Decide, whether to scream or not
+    
+    Returns:
+        str: May be in caps lock
 
     """
     if do_scream:
